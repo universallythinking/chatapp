1af895f7a9c3886e7abf822508362d8190a49735
Ben Barnett <benjamin@UniversallyThinking.com>
2016-07-19 15:24:49 -0400

    Initial

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/ejs/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/gravatar/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/socket.io/index.d.ts" />
